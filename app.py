b9581de2658392f981102af8dfda606b78d14fd2
MiguelAngelZ1 <miguelanyelo0011@gmail.com>
2025-05-06 20:38:46 +0000

    Auto commit: 2025-05-06 20:38:46

--- app.py
+++ app.py
@@ -139,4 +139,5 @@ def delete():
     return render_template("delete.html")
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=False)
+    app.run(host="0.0.0.0", port=5000, debug=True)
+
