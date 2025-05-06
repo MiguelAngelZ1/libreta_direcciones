7836a313a1f1096db92373c6cf22cc3f0d273a7d
MiguelAngelZ1 <miguelanyelo0011@gmail.com>
2025-05-06 22:55:53 +0000

    Auto commit: 2025-05-06 22:55:53

--- app.py
+++ app.py
@@ -51,8 +51,8 @@ def add():
     
     # Validación del DNI: debe ser numérico y tener al menos 8 dígitos.
     if not dni.isdigit() or len(dni) != 8:
-    error = "El DNI debe contener solo números y tener exactamente 8 dígitos."
-    return render_template("add.html", error=error)
+        error = "El DNI debe contener solo números y tener exactamente 8 dígitos."
+        return render_template("add.html", error=error)
 
 
     # Conexión a la base de datos
