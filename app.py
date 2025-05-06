73d842113d9c9f028c6f9165f26044dee127c477
MiguelAngelZ1 <miguelanyelo0011@gmail.com>
2025-05-06 22:30:35 +0000

    Auto commit: 2025-05-06 22:30:35

--- app.py
+++ app.py
@@ -113,7 +113,7 @@ def edit():
 
     # Validación para el DNI: debe contener solo números y tener al menos 8 dígitos.
     if not nuevo_dni.isdigit() or len(nuevo_dni) < 8:
-    flash("Error: DNI debe contener solo números y tener al menos 8 dígitos.", "danger")
+        flash("Error: DNI debe contener solo números y tener al menos 8 dígitos.", "danger")
     return redirect("/view")
 
     
